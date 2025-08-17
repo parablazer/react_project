9993d3bc6e39a5272a82c34934582cf1a574e0eb
Dominik Rudnicki <dominik@tncisp.com>
2025-08-17 15:28:12 -0500

    Import the photos

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,17 +1,21 @@
+import FaceDominik from '../assets/Face-Dominik.png'
+import MailIcon from '../assets/mail-icon.png'
+import PhoneIcon from '../assets/phone-icon.png'
+
 export function Contact (){
     return(
             <article className="contact-card">
                 <img
-                src="/public/Face-Dominik.png"
+                src={FaceDominik}
                 alt="Dominik Rudnicki"
                 ></img>
                 <h3>Dominik Rudnicki</h3>
                 <div className="info-group">
-                    <img src="/public/phone-icon.png" alt="Phone Icon"></img>
+                    <img src={PhoneIcon} alt="Phone Icon"></img>
                     <p>281-740-1011</p>
                 </div>
                 <div className="info-group">
-                <img src="/public/mail-icon.png" className="icon" alt="Phone Icon"></img>
+                <img src={MailIcon} className="icon" alt="Phone Icon"></img>
                 <p>ewelina@tncisp.com</p>
                 </div>
             </article>
