6e3dc58f1793cbbf10e198ed61ec7c9ee5a756c7
Dominik Rudnicki <dominik@tncisp.com>
2025-08-17 15:10:51 -0500

    Removing it from all images

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,16 +2,16 @@ export function Contact (){
     return(
             <article className="contact-card">
                 <img
-                src="./src/assets/Face-Dominik.png"
+                src="/assets/Face-Dominik.png"
                 alt="Dominik Rudnicki"
                 ></img>
                 <h3>Dominik Rudnicki</h3>
                 <div className="info-group">
-                    <img src="./assets/phone-icon.png" alt="Phone Icon"></img>
+                    <img src="/assets/phone-icon.png" alt="Phone Icon"></img>
                     <p>281-740-1011</p>
                 </div>
                 <div className="info-group">
-                <img src="./assets/mail-icon.png" className="icon" alt="Phone Icon"></img>
+                <img src="/assets/mail-icon.png" className="icon" alt="Phone Icon"></img>
                 <p>ewelina@tncisp.com</p>
                 </div>
             </article>
