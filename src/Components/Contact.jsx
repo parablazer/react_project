a97200d7c90b693c4f75265ff8b846a6fc7f807b
Dominik Rudnicki <dominik@tncisp.com>
2025-08-17 15:20:20 -0500

    update location of images

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,16 +2,16 @@ export function Contact (){
     return(
             <article className="contact-card">
                 <img
-                src="assets/Face-Dominik.png"
+                src="public/Face-Dominik.png"
                 alt="Dominik Rudnicki"
                 ></img>
                 <h3>Dominik Rudnicki</h3>
                 <div className="info-group">
-                    <img src="assets/phone-icon.png" alt="Phone Icon"></img>
+                    <img src="public/phone-icon.png" alt="Phone Icon"></img>
                     <p>281-740-1011</p>
                 </div>
                 <div className="info-group">
-                <img src="assets/mail-icon.png" className="icon" alt="Phone Icon"></img>
+                <img src="public/mail-icon.png" className="icon" alt="Phone Icon"></img>
                 <p>ewelina@tncisp.com</p>
                 </div>
             </article>
